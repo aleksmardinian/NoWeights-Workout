0e351e18e05269528bb66d144560bb2e458cdbfb
Aleksander Mardinian <aleksmardinian@gmail.com>
2020-04-17 21:04:33 +0100

    fixed drag and drop

--- js/java.js
+++ js/java.js
@@ -1,19 +1,29 @@
 // alert("this alert box");
-// console.log(text);
-// console.log(index);
 
 var forms = document.querySelectorAll('.new-exercise');
 var inputs = document.querySelectorAll('.new-exercise-input');
 var exercises = document.querySelectorAll('.exercise-list');
 var workouts = document.querySelectorAll('.workout');
-
+var draggableExercises = document.querySelectorAll('.item li, .item form');
+var cardHeight;
 var exerciseList = [];
-
 var sidebarToggler = document.querySelector('#sidebar-toggler');
+
 sidebarToggler.addEventListener('click', event => {
 	toggleSidebar();
 	}
 );
+
+dragCheck();
+
+function recalculate() {
+	forms = document.querySelectorAll('.new-exercise');
+	inputs = document.querySelectorAll('.new-exercise-input');
+	exercises = document.querySelectorAll('.exercise-list');
+	workouts = document.querySelectorAll('.workout');
+	draggableExercises = document.querySelectorAll('.item li, .item form');
+	dragCheck();
+}
 
 document.addEventListener('submit', event => {
 	forms.forEach((form, index) => {
@@ -31,12 +41,6 @@ document.querySelector('.add-workout').addEventListener('click', event => {
 	addWorkout();
 });
 
-function recalculate() {
-	forms = document.querySelectorAll('.new-exercise');
-	inputs = document.querySelectorAll('.new-exercise-input');
-	exercises = document.querySelectorAll('.exercise-list');
-	workouts = document.querySelectorAll('.workout');
-}
 
 function toggleSidebar() {
 	var sidebar = document.querySelector('.exercises');
@@ -51,27 +55,41 @@ function toggleSidebar() {
 }
 
 
+// ADD EXERCISE
+
+
 function addExercise(text, index) {   
 
-	 var exercise = {
-	 	text: text,
-	 	reps: 0,
-	 	time: "",
-	 	id: Date.now()
-	 };
+	var exercise = {
+		text: text,
+		reps: 0,
+		time: "",
+		id: Date.now()
+	};
 
-	 exerciseList.push(exercise);
+	workout = exercises[index];
 
-	 exercises[index].insertAdjacentHTML('beforeEnd', 
-	 	'<li data-key=' + exercise.id + '>' +
-			exercise.text +
-		'</li>'
-	 	);
+	exerciseList.push(exercise);
 
-     var scrollExercises = document.querySelectorAll(".scroll-exercises");
-  
-     scrollExercises[index].scrollTop = scrollExercises[index].scrollHeight;
+	var exerciseHTML =	`<li data-key= ${exercise.id} draggable="true"> ${exercise.text} </li>`
+	var formHTML = exercises[index].querySelector('.new-exercise');
+
+	console.log(exerciseHTML);
+	console.log(formHTML);
+	console.log(exercises);
+	console.log(index);
+
+	formHTML.insertAdjacentHTML('beforebegin', exerciseHTML);
+
+    var scrollExercises = document.querySelectorAll(".scroll-exercises");
+    scrollExercises[index].scrollTop = scrollExercises[index].scrollHeight;
+
+    recalculate();
 }
+
+
+// ADD WORKOUT
+
 
 function addWorkout(){
 
@@ -82,13 +100,14 @@ function addWorkout(){
 		<h4>Workout ${numberWorkouts+1}</h4>
 		<div class="scroll-exercises">
 			<ul class="list-unstyled exercise-list">
-			</ul>
-			<form class="new-exercise">
-				<input type="text" aria-label="New exercise" placeholder="New exercise" class="new-exercise-input">
+				<form class="new-exercise">
+					<input type="text" aria-label="New exercise" placeholder="New exercise" class="new-exercise-input">
 				</form>
-			</div>
-		</div>`
-		);
+			</ul>
+		</div>
+	</div>`
+	);
+
 	recalculate();
 
 	var numberInputs = inputs.length;
@@ -101,55 +120,59 @@ function addWorkout(){
 
 
 
+// DRAGGING
+
+function dragCheck(){
+	draggableExercises.forEach((exercise, index) => {
+		exercise.addEventListener('dragstart', dragStart);
+		exercise.addEventListener('dragend', event => dragEnd(exercise));
+		exercise.addEventListener('dragenter', event => dragEnter(event, exercise));
+	});
+}
+
+function dragStart() {
+	cardHeight = this.offsetHeight;
+	setTimeout(() => (this.classList.add('invisible')), 0);
+}
+
+function dragEnd(exercise) {
+	exercise.classList.remove('invisible');
+	var cardPlaceholder = document.querySelector('.card-placeholder');
+
+	if (cardPlaceholder!= undefined){
+		// cardPlaceholder.parentNode.insertBefore(exercise, null)
+		cardPlaceholder.replaceWith(exercise);
+	}
+}
+
+function dragEnter(event, exercise) {
+	var cardPlaceholderHTML = `<li class="card-placeholder" style="height:${cardHeight}px"></li>`;
+
+	event.preventDefault();
+	if (document.querySelector('.card-placeholder') == undefined){
+		exercise.insertAdjacentHTML('beforebegin', cardPlaceholderHTML);
+		console.log(exercise);
+	}
+	var cardPlaceholder = document.querySelector('.card-placeholder');
+	cardPlaceholder.addEventListener('dragleave', event => dragLeave());
+	cardPlaceholder.addEventListener('dragover', event => dragOver(event));
+	cardPlaceholder.addEventListener('dragdrop', event => dragDrop());
+}
 
 
-var draggableExercises = document.querySelectorAll(".item li");
+function dragOver(event) {
+	event.preventDefault();
+}
 
-console.log(draggableExercises);
+function dragLeave() {
+	console.log(1)
+	if (document.querySelector('.card-placeholder') != undefined){
+		document.querySelector('.card-placeholder').remove();
+	}
+}
 
-draggableExercises.forEach((exercise, index) => {
-	dragElement(draggableExercises[index])
-});
-
-function dragElement(element) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(element.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(element.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    element.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    element.style.top = (element.offsetTop - pos2) + "px";
-    element.style.left = (element.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+function dragDrop() {
+	console.log("DROPPED");
 }
 
 
